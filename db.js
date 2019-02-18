c8e3d3c39c43e97858a052feff7131b19709debb
Rabeeh <rabeehrz@instagram.com>
2019-02-18 19:49:01 +0530

    Added Todo basic functions

--- db.js
+++ db.js
@@ -4,34 +4,80 @@ let connection= new sequelize('user_db','user','password',{
 	host:'localhost',
 	dialect:'mysql'
 });
+
 let User=connection.define('users',{
 	name:{
 		type:sequelize.STRING,
-		unique:true
+		unique:true,
+		validate:{
+			notEmpty: {
+				args: true,
+				msg: "Username field cannot be empty"
+			}
+		}
 	},
 	password:{
-		type:sequelize.STRING
+		type:sequelize.STRING,
+		validate:{
+			notEmpty: {
+				args: true,
+				msg: "Password field cannot be empty"
+			}
+		}
 	}
 },{
 	freezeTableName: true
 });
-let a={
-	createUser: (user)=>{
-		connection
-		.sync({
-			logging:console.log("logged")
-		})
-		.then(()=>{
-			User.create({
-				name:user.email,
-				password:user.pwd
 
-			})
-		})
-		.catch((err)=>{
-			console.log("Error creating database");
+let Todo=connection.define('todos',{
+	userid:{
+		type:sequelize.INTEGER,
+	},
+	task: {
+		type: sequelize.TEXT,
+	},
+	completed: {
+		type: sequelize.BOOLEAN,
+		defaultValue: false
+	}
+},{
+	freezeTableName: true
+});
+
+connection.sync();
+
+let a = {
+	createUser: (user, result)=>{
+		User.create({
+			name:user.email,
+			password:user.pwd
+		}).then(() => {
+			return result(null);
+		}).catch((err) => {
+			return result(err.message);
 		});
 		return true;
+	},
+	addTask: (task, result) => {
+		Todo.create({
+			userid: 1,
+			task: task.task
+		}).then(() => {
+			return result(null);
+		}).catch((error) => {
+			return result(error.message)
+		});
+	},
+	toggleTask: (task, result) => {
+		Todo.update({
+			completed: task.status
+		},{
+			id: task.id
+		}).then(() => {
+			return result(null);
+		}).catch((error) => {
+			return result(error.message)
+		});
 	}
 }
 
--- server.js
+++ server.js
@@ -12,15 +12,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/action',(req,res)=>{	
-		if(db.a.createUser(req.body)){
-			res.redirect('/login');
-		}
+		// if(db.a.createUser(req.body)){
+		// 	res.redirect('/login');
+		// }
+		db.a.createUser(req.body, (error) => {
+			if (!error) {
+				res.redirect('/login');
+			} else {
+				console.log(error);
+			}
+		});
 });
 app.get('/signup',(req,res)=>{
 	res.render('signup.hbs')
 })
 app.get('/login',(req,res)=>{
 	res.render('login.hbs')
+})
+app.get('/todo',(req,res)=>{
+	res.render('todo.hbs')
+})
+app.post('/todo', (req,res) => {
+	db.a.addTask(req.body, (error) => {
+		if (!error) {
+			res.redirect('/todo');
+		} else {
+			console.log(error);
+		}
+	});
 })
 app.listen(3000);
 
